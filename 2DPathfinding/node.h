6fa750a9577a0fee546b285c8d647a950f3ae7a3
Steven Barrett <barrett1733@sbcglobal.net>
2015-04-10 21:00:54 -0700

    Removed unused variables.

--- 2DPathfinding/node.h
+++ 2DPathfinding/node.h
@@ -7,20 +7,17 @@
 struct Node {
 	Position pos;
 	Node* parentNode;
-	bool visited = false;
-	double gcost, hcost, fcost;
+	double gcost, fcost;
 	Node() :
 		pos(Position(0, 0)),
 		parentNode(NULL),
 		gcost(0),
-		hcost(0),
 		fcost(0)
 	{}
 	Node(Position pos, Node* parent, double g, double h) :
 		pos(pos),
 		parentNode(parent),
 		gcost(g),
-		hcost(h),
 		fcost(g + h)
 	{}
 	void set(Position pos_, Node* par, double g, double h)
@@ -28,7 +25,6 @@ struct Node {
 		pos = pos_;
 		parentNode = par;
 		gcost = g;
-		hcost = h;
 		fcost = g + h;
 	}
 	bool operator== (Node &a) const
@@ -36,26 +32,22 @@ struct Node {
 		return (pos == a.pos &&
 			parentNode == a.parentNode &&
 			gcost == a.gcost &&
-			hcost == a.hcost &&
-			fcost == a.fcost &&
-			visited == a.visited);
+			fcost == a.fcost);
 	}
 	Node& operator= (Node& a)
 	{
 		pos = a.pos;
 		parentNode = a.parentNode;
 		gcost = a.gcost;
-		hcost = a.hcost;
 		fcost = a.fcost;
-		visited = a.visited;
 		return *this;
 	}
 	friend std::ostream& operator<<(std::ostream& os, const Node& node)
 	{
 		if (node.parentNode != NULL)
-			os << node.pos << " " << node.parentNode->pos << " G:" << node.gcost << " H:" << node.hcost << " F:" << node.fcost;
+			os << node.pos << " " << node.parentNode->pos << " G:" << node.gcost << " F:" << node.fcost;
 		else
-			os << node.pos << " (N,N)" << " G:" << node.gcost << " H:" << node.hcost << " F:" << node.fcost;
+			os << node.pos << " (N,N)" << " G:" << node.gcost << " F:" << node.fcost;
 		return os;
 	}
 };
